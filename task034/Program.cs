585aa11cbb21627d9683142abd8e1378e9bb2c8e
yuliagorgun <yuliashkelenok@gmail.com>
2022-11-17 14:39:06 +0300

    Выполнено 36 задание

---
+++ task034/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int length = 4;
+int[] massive = new int [length];
+FillArray(massive);
+PrintArray(massive);
+
+void FillArray(int[] array, int minValue = 100 ,int maxValue = 999)
+{
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue,maxValue+1);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int count = 0;
+for (int Z = 0; Z < massive.Length; Z++)
+if (massive[Z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {massive.Length} чисел, {count} из них чётные");
---
+++ task036/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+int length = 4;
+int[] massive = new int [length];
+FillArray(massive);
+PrintArray(massive);
+int sum = 0;
+for (int i = 0; i < massive.Length; i+=2)
+sum = sum + massive[i];
+
+Console.WriteLine($"всего {massive.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+
+void FillArray(int[] array, int minValue = 1 ,int maxValue = 100)
+{
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue,maxValue+1);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
